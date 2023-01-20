9f009f1b2fd51b8f9983625641a13a52f5ba8a37
Xinmeng <xc139@duke.edu>
2023-01-20 01:47:58 -0500

    add and pass my simple FF test

--- project1-kit/my_malloc/my_malloc.c
+++ project1-kit/my_malloc/my_malloc.c
@@ -55,6 +55,7 @@ void * ff_malloc(size_t size) {
     res = sbrk((size + 1) * SAVE_UNIT);
     *res = (size_t)(res + size + 1);
     total_size += (size + 1) * SAVE_UNIT;
+    // printf("%lu\n",total_size);
     return res + 1;
   }
   return res;
@@ -114,3 +115,62 @@ unsigned long get_data_segment_free_space_size() {
   }
   return res;
 }
+
+// int main(void){
+// void* start=sbrk(0);
+// void* end;
+
+// size_t expected=(size_t)start;
+// size_t actual;
+// size_t usable=0;
+// int* a[10];
+// size_t addsave[10];
+// for(int i=1;i<=10;i++){
+//    a[i-1]= (int*)ff_malloc(i*sizeof(int));
+//    expected+=i*sizeof(int)+sizeof(size_t*);
+//    for(int j=0;j<i;j++){
+//     a[i-1][j]=1;
+//    }
+// }
+// int sum=0;
+// for(int i=0;i<10;i++){
+//   for(int j=0;j<=i;j++){
+//     sum+=a[i][j];
+//   }
+// }
+// for(int i=0;i<3;i++){
+//   ff_free(a[i]);
+// }
+// end=sbrk(0);
+// addsave[0]=(size_t)end;
+// int * new_ptr[2];
+// new_ptr[0]=(int*)ff_malloc(2*sizeof(int));
+// end=sbrk(0);
+// addsave[1]=(size_t)end;
+// ff_free(a[3]);
+// new_ptr[1]=(int*)ff_malloc(8*sizeof(int)-sizeof(size_t*));
+// end=sbrk(0);
+// addsave[2]=(size_t)end;
+// ff_free(new_ptr[0]);
+// ff_free(new_ptr[1]);
+// for(int i=4;i<10;i++){
+//   ff_free(a[i]);
+// }
+// size_t int_size=sizeof(int);
+// void * final_pointer=ff_malloc(55*sizeof(int)+9*sizeof(size_t*));
+// end=sbrk(0);
+// addsave[3]=(size_t)end;
+// ff_free(final_pointer);
+// unsigned long empty_size= get_data_segment_free_space_size();
+// end=sbrk(0);
+// printf("start pointer:  %lu\n",(size_t)start);
+// printf("Sum result: %d \n",sum);
+// printf("Empty size:  %lu Int size %lu\n",empty_size,int_size);
+// for(int i=0;i<4;i++){
+//   printf("Save %d is %lu\n",i,addsave[i]);
+// }
+// printf("Expected Address: %lu, Actual Address: %lu\n",expected,(size_t)end); 
+// printf("Calculated size: %lu  Actual size: %lu\n",total_size,(size_t)end-(size_t)start);
+// return 0;
+
+// }
