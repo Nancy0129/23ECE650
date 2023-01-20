359657ea730aaed0ce09a866cead667f229af9d0
Xinmeng <xc139@duke.edu>
2023-01-20 16:29:25 -0500

    after TA

--- project1-kit/my_malloc/alloc_policy_tests/small_range_rand_allocs.c
+++ project1-kit/my_malloc/alloc_policy_tests/small_range_rand_allocs.c
@@ -1,36 +1,66 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
-
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
 #include "my_malloc.h"
 
-#define NUM_ITERS 100
-#define NUM_ITEMS 10000
+#define NUM_ITERS    100
+#define NUM_ITEMS    10000
 
 #ifdef FF
 #define MALLOC(sz) ff_malloc(sz)
-#define FREE(p) ff_free(p)
+#define FREE(p)    ff_free(p)
 #endif
 #ifdef BF
 #define MALLOC(sz) bf_malloc(sz)
-#define FREE(p) bf_free(p)
+#define FREE(p)    bf_free(p)
 #endif
 
+char dumb_hash(void* p) {
+    unsigned char h = (unsigned char) (17*(uintptr_t)p*(uintptr_t)p + 7*(uintptr_t)p + 3*((uintptr_t)p>>8)); // dumb garbage fake hash
+    return h;
+}
+
+void* MALLOC_WITH_CHECK(size_t sz) {
+    void* p = MALLOC(sz);
+    unsigned char h = dumb_hash(p);
+    memset(p,h,sz);
+    return p;
+}
+
+bool FREE_WITH_CHECK(void* p, size_t sz) {
+    bool r = true;
+    unsigned char h = dumb_hash(p);
+    int i;
+    unsigned char* cp = p;
+    for (i=0; i<sz; i++, cp++) {
+        if (*cp != h) {
+            r = false;
+            //printf("p=%p cp=%p *cp=%x h=%x\n",p,cp,*cp,h);
+            break;
+        }
+    }
+    FREE(p);
+    return r;
+}
+
 double calc_time(struct timespec start, struct timespec end) {
-  double start_sec = (double)start.tv_sec * 1000000000.0 + (double)start.tv_nsec;
-  double end_sec = (double)end.tv_sec * 1000000000.0 + (double)end.tv_nsec;
+  double start_sec = (double)start.tv_sec*1000000000.0 + (double)start.tv_nsec;
+  double end_sec = (double)end.tv_sec*1000000000.0 + (double)end.tv_nsec;
 
   if (end_sec < start_sec) {
     return 0;
-  }
-  else {
+  } else {
     return end_sec - start_sec;
   }
 };
 
+
 struct malloc_list {
   size_t bytes;
-  int * address;
+  int *address;
 };
 typedef struct malloc_list malloc_list_t;
 
@@ -38,27 +68,36 @@ malloc_list_t malloc_items[2][NUM_ITEMS];
 
 unsigned free_list[NUM_ITEMS];
 
-int main(int argc, char * argv[]) {
+
+int main(int argc, char *argv[])
+{
   int i, j, k;
   unsigned tmp;
   unsigned long data_segment_size;
   unsigned long data_segment_free_space;
   struct timespec start_time, end_time;
+  
+  int num_iters = NUM_ITERS;
+  if (argc>=2) {
+      num_iters = atoi(argv[1]);
+      if (num_iters==0) {
+          printf("Syntax: %s [num_iters_override]\n",argv[0]);
+          return 1;
+      }
+      num_iters &= -2; // make it a multiple of 2
+  }
 
   srand(0);
-  // printf("0\n");
+
   const unsigned chunk_size = 32;
   const unsigned min_chunks = 4;
   const unsigned max_chunks = 16;
-  for (i = 0; i < NUM_ITEMS; i++) {
-    malloc_items[0][i].bytes =
-        ((rand() % (max_chunks - min_chunks + 1)) + min_chunks) * chunk_size;
-    malloc_items[1][i].bytes =
-        ((rand() % (max_chunks - min_chunks + 1)) + min_chunks) * chunk_size;
+  for (i=0; i < NUM_ITEMS; i++) {
+    malloc_items[0][i].bytes = ((rand() % (max_chunks - min_chunks + 1)) + min_chunks) * chunk_size;
+    malloc_items[1][i].bytes = ((rand() % (max_chunks - min_chunks + 1)) + min_chunks) * chunk_size;
     free_list[i] = i;
-  }  //for i
+  } //for i
 
-  // printf("\n1\n");
   i = NUM_ITEMS;
   while (i > 1) {
     i--;
@@ -66,60 +105,53 @@ int main(int argc, char * argv[]) {
     tmp = free_list[i];
     free_list[i] = free_list[j];
     free_list[j] = tmp;
-  }  //while
+  } //while
 
-  // printf("\n2\n");
-  for (i = 0; i < NUM_ITEMS; i++) {
-    malloc_items[0][i].address = (int *)MALLOC(malloc_items[0][i].bytes);
-  }  //for i
 
-  // printf("\n3\n");
+  for (i=0; i < NUM_ITEMS; i++) {
+    malloc_items[0][i].address = (int *)MALLOC_WITH_CHECK(malloc_items[0][i].bytes);
+  } //for i
+
+  bool r = false;
+
   //Start Time
   clock_gettime(CLOCK_MONOTONIC, &start_time);
-
-  for (i = 0; i < NUM_ITERS; i++) {
+  
+  for (i=0; i < num_iters; i++) {
     unsigned malloc_set = i % 2;
-    
-    for (j = 0; j < NUM_ITEMS; j += 50) {
-      // printf("  %d  %d\n",i,j);
-      for (k = 0; k < 50; k++) {
-        unsigned item_to_free = free_list[j + k];
-        FREE(malloc_items[malloc_set][item_to_free].address);
-      }  //for k
-          
-      for (k = 0; k < 50; k++) {
-         
-        
-        malloc_items[1 - malloc_set][j + k].address =
-            (int *)MALLOC(malloc_items[1 - malloc_set][j + k].bytes);
-            
-      }
-      // printf("\n done k2\n");
-      
-        //for k
-    }  
-    
-      //for j
-  }      //for i
+    printf("\rIteration %5d / %5d (%.1f%%)", i, num_iters, (float)i/num_iters*100); fflush(stdout);
+    for (j=0; j < NUM_ITEMS; j+=50) {
+      for (k=0; k < 50; k++) {
+        unsigned item_to_free = free_list[j+k];
+        r = FREE_WITH_CHECK(malloc_items[malloc_set][item_to_free].address, malloc_items[malloc_set][item_to_free].bytes);
+        if (!r) {
+          printf("ERROR: Content check failed while freeing %p\n", malloc_items[malloc_set][item_to_free].address);
+        }
+      } //for k
+      for (k=0; k < 50; k++) {
+        malloc_items[1-malloc_set][j+k].address = (int *)MALLOC_WITH_CHECK(malloc_items[1-malloc_set][j+k].bytes);
+      } //for k
+    } //for j
+  } //for i
+  printf("\n\n");
 
-  // printf("\n \n 4\n");
   //Stop Time
   clock_gettime(CLOCK_MONOTONIC, &end_time);
 
   data_segment_size = get_data_segment_size();
   data_segment_free_space = get_data_segment_free_space_size();
-  printf("data_segment_size = %lu, data_segment_free_space = %lu\n",
-         data_segment_size,
-         data_segment_free_space);
+  printf("data_segment_size = %lu, data_segment_free_space = %lu\n", data_segment_size, data_segment_free_space);
 
   double elapsed_ns = calc_time(start_time, end_time);
   printf("Execution Time = %f seconds\n", elapsed_ns / 1e9);
-  printf("Fragmentation  = %f\n",
-         (float)data_segment_free_space / (float)data_segment_size);
+  printf("Fragmentation  = %f\n", (float)data_segment_free_space/(float)data_segment_size);
 
-  for (i = 0; i < NUM_ITEMS; i++) {
-    FREE(malloc_items[0][i].address);
-  }  //for i
+  for (i=0; i < NUM_ITEMS; i++) {
+    r = FREE_WITH_CHECK(malloc_items[0][i].address, malloc_items[0][i].bytes);
+    if (!r) {
+      printf("ERROR: Content check failed while freeing %p\n", malloc_items[0][i].address);
+    }
+  } //for i
 
   return 0;
 }
--- project1-kit/my_malloc/my_malloc.c
+++ project1-kit/my_malloc/my_malloc.c
@@ -9,7 +9,6 @@ size_t get_size(size_t in_size) {
 }
 void * cut_space(size_t * pre, size_t * curr, size_t target,size_t curr_size) {
   if (curr_size-target < 2) {
-    // printf("%lu  %lu",)
     if (pre == free_head) {
       free_head = (size_t *)*(curr + 1);
     }
@@ -53,9 +52,11 @@ void * ff_malloc(size_t size) {
   size_t * res = find_ff(size);
   if (res == NULL) {
     res = sbrk((size + 1) * SAVE_UNIT);
+    if((void*)res==(void*)-1){
+      return NULL;
+    }
     *res = (size_t)(res + size + 1);
     total_size += (size + 1) * SAVE_UNIT;
-    // printf("%lu\n",total_size);
     return res + 1;
   }
   return res;
@@ -65,7 +66,6 @@ void ff_free(void * ptr) {
   if(ptr==NULL){
     return;
   }
-  // printf("2");
   size_t * to_free = (size_t *)ptr - 1;
   if (free_head == NULL) {
     *(to_free + 1) = (size_t)NULL;
@@ -105,20 +105,25 @@ unsigned long get_data_segment_size() {
 }
 
 unsigned long get_data_segment_free_space_size() {
-  // printf("3");
   unsigned long res = 0;
   size_t * curr = free_head;
   while (curr != NULL) {
     res += *curr - (unsigned long)curr;
     curr = (size_t *)*(curr + 1);
-    //printf("%lu,   %lu\n", res, (size_t)curr);
   }
   return res;
 }
 
+// For testing
 // int main(void){
 // void* start=sbrk(0);
+// void* med = sbrk(20);
 // void* end;
+// end=sbrk(0);
+// printf("%lu\n",(size_t)end);
+// end=sbrk(0);
+// printf("%lu\n",(size_t)end);
+
 
 // size_t expected=(size_t)start;
 // size_t actual;
@@ -174,3 +179,55 @@ unsigned long get_data_segment_free_space_size() {
 // return 0;
 
 // }
+
+void bf_free(void * ptr) {
+  if(ptr==NULL){
+    return;
+  }
+  size_t * to_free = (size_t *)ptr - 1;
+  if (free_head == NULL) {
+    *(to_free + 1) = (size_t)NULL;
+    free_head = to_free;
+    return;
+  }
+  size_t * curr = free_head;
+  size_t * prev = free_head;
+  while (curr != NULL && (size_t)curr < (size_t)to_free) {
+    prev = curr;
+    curr = (size_t *)*(curr + 1);
+  }
+  if (*prev == (size_t)to_free) {
+    *prev = *to_free;
+    to_free = prev;
+  }
+  else {
+    if (prev == free_head) {
+      *(to_free + 1) = (size_t)free_head;
+      free_head = to_free;
+    }
+
+    else {
+      *(to_free + 1) = (size_t)curr;
+      *(prev + 1) = (size_t)to_free;
+    }
+  }
+
+  if (*to_free == (size_t)curr) {
+    *(to_free + 1) = *(curr + 1);
+    *to_free = *curr;
+  }
+}
+void * bf_malloc(size_t size) {
+  size = get_size(size);
+  size_t * res = find_ff(size);
+  if (res == NULL) {
+    res = sbrk((size + 1) * SAVE_UNIT);
+    if((void*)res==(void*)-1){
+      return NULL;
+    }
+    *res = (size_t)(res + size + 1);
+    total_size += (size + 1) * SAVE_UNIT;
+    return res + 1;
+  }
+  return res;
+}
